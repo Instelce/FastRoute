6acf35fed88fc52944cd384f6e40369bae24de65
Instelce <instelce@protonmail.com>
2022-06-19 15:55:46 +0200

    Deleting all maps and tilesets

--- maps_data/tilesets/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="16" tileheight="16" tilecount="504" columns="24">
- <image source="../../graphics/terrain/tiles.png" width="384" height="336"/>
-</tileset>
